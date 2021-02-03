b10eeed95ae17e62145b53b3278c4e78cf546851
alexm323 <alejandromartinezjr42@gmail.com>
2021-02-02 16:47:08 -0800

    finalized notes on useEffect and useRef hooks

--- use_effect_hook/src/App.js
+++ use_effect_hook/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Counter from './Counter'
+import ProfileViewer from './ProfileViewer';
+import ProfileViewerWithSearch from './ProfileViewerWithSearch';
+import Timer from './Timer'
+import Video from './Video'
+import TimerWrapper from './TimerWrapper'
+import Focuser from './Focuser'
+import Timer2  from './Timer2'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <ProfileViewerWithSearch/> */}
+      {/* <TimerWrapper/> */}
+      {/* <Video/> */}
+      <Timer2/>
     </div>
+
   );
 }
 
---
+++ use_effect_hook/src/ProfileViewer.js
@@ -0,0 +1,26 @@
+import React,{useState, useEffect} from "react";
+
+import axios from 'axios';
+
+// "https://api.github.com/users/elie"
+
+const ProfileViewer = ({name='Elie',color='purple'}) => {
+    const [data,setData] = useState(null);
+    useEffect(()=>{
+        async function loadProfile(){
+            console.log(`Making a new http request`)
+            const res = await axios.get(`https://api.github.com/users/${name}`);
+            setData(res.data.name)
+        }
+        loadProfile();
+    },[name])
+    
+
+    return(
+        <div>
+            <h3 style={{color}}>{data ? data : 'Loading...'}</h3>
+        </div>
+    )
+};
+
+export default ProfileViewer;
